42c7fffc848b3355d8b1924ddc530ce0428f6f91
122208997 <lehaohust@gmail.com>
2019-03-12 04:40:26 +0700

    FEATURE: CREACTE. 0. New Item was added display on top successfully

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -12,17 +12,27 @@ export class NoteService {
 		new Note(3, 'Shecodes Team Meeting', 'Worktitle in Slack, choose Agenda for Event')
 	]
 	constructor() { }
-
-	getAllNotes() {
-		return this.notes;
-	}
 	// After that, go to App Module to declare wihtin "provider"
 
 	// After those :v, go to noteList to use :3
 
+	getAllNotes() {
+		return this.notes;
+	}
+
 	addNote( note: Note) {
 		note.id = this.getLastID(this.notes) + 1;
 		this.notes.push(note);
+		this.notes.sort((a, b) => {
+			if (a.id > b.id) {
+				return -1; 
+			}
+			else if (a.id < b.id) {
+				return 1; 
+			}
+			return 0
+		})
+		// console.log(this.notes);
 	}
 
 	getLastID(notes) {
