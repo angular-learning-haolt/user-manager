2fd5fc8122fc039b4b0bf5d5c225952a2d494cec
122208997 <lehaohust@gmail.com>
2019-03-11 23:03:56 +0700

    create model: Note.class

---
+++ src/app/models/note.class.ts
@@ -0,0 +1,12 @@
+export class Note {
+	
+	public id : number;
+	public title : string;
+	public content : string;
+
+	constructor(id: number, title: string, content: string, ) {
+		this.id = id;
+		this.title = title;
+		this.content = content;
+	}
+}
