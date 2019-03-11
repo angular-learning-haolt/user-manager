0b1615dc23ab9e80a00464601b5d1f23ccd35981
122208997 <lehaohust@gmail.com>
2019-03-11 22:56:43 +0700

    base layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+import { NoteItemComponent } from './components/note-item/note-item.component';
+import { NoteFormComponent } from './components/note-form/note-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteListComponent,
+    NoteItemComponent,
+    NoteFormComponent
   ],
   imports: [
     BrowserModule,
